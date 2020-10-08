6c99b214834e5751654a6cf0e3b5932ecaf6b373
zaw83 <ktmthar@gmail.com>
2020-10-08 23:18:36 +0630

    081020202318 Night Laptop Commit

--- djapp/accounts/urls.py
+++ djapp/accounts/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('product/', views.product),
-    path('customer/', views.customer),
+    path('', views.home, name='home'),
+    path('product/', views.product, name='product'),
+    path('customer/<str:pk>', views.customer, name='customer'),
     
+    path('create_order/',views.createOrder, name='create_order'),
+    path('update_order/<str:pk>',views.updateOrder, name='update_order'),
+    path('create_customer/', views.createCustomer, name='create_customer'),
+    path('delete_order/<str:pk>', views.deleteOrder, name='delete_order'),
 ]
--- djapp/accounts/views.py
+++ djapp/accounts/views.py
@@ -1,13 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from .forms import OrderForm, CustomerForm
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders':orders, 'customers':customers,
+                'total_customers':total_customers, 'total_orders':total_orders,
+                'delivered': delivered, 'pending':pending}
+    return render(request, 'accounts/dashboard.html', context)
 
 def product(request):
-    return render(request, 'accounts/product.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/product.html', {'products':products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    
+    orders= customer.order_set.all()
+    total_orders = orders.count()
+    context = {'customer':customer, 'orders':orders, 'total_orders':total_orders}
+    return render(request, 'accounts/customer.html', context)
 
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        print('Printing POST:', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
 
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        print('Printing POST:', request.POST)
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def createCustomer(request):
+    form = CustomerForm()
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
+    return render(request, 'accounts/customer_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+    context = {'item':order}
+    return render(request, 'accounts/delete.html',context)
